4b649b61950cab1bc87c4652aebda45b8c511f36
Eduardo Dominguez Sanchez <zancheze559@gmail.com>
2024-12-07 14:24:23 -0600

    validacion.py

---
+++ validacion.py
@@ -0,0 +1,9 @@
+user1 = "lanselol"
+contraseña = "lans56"
+user =input("Ingresa tu usuario:")
+contraseña1 =input("Ingresa tu contraseña:")
+if user == user1 and contraseña == contraseña1:
+    print("Bienvenido de vuelta")
+else:
+    print("Aceeso denegado verifica tus datos")
+
